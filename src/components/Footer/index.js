8913c4dbf0dd4d036aaf1ddffecb6106c629e82d
Aryan Maurya <aryan7860923150@gmail.com>
2024-03-16 11:29:01 +0530

    small changes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -88,7 +88,7 @@ function Footer() {
   return (
     <FooterContainer>
       <FooterWrapper>
-        <Logo>ARYAN KUMAR MAURYA</Logo>
+        <Logo>ARYAN MAURYA</Logo>
         <Nav>
           <NavLink href="#about">About</NavLink>
           <NavLink href="#skills">Skills</NavLink>
